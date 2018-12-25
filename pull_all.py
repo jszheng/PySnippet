34cec3612f3d4afb381117b0d95f13c182db6ff0
jszheng <zheng_js@hotmail.com>
2018-12-25 16:46:33 +0800

    back to old python version for compatibilities

--- pull_all.py
+++ pull_all.py
@@ -3,48 +3,33 @@ import sys
 
 path_done = set()
 
+
 # copy from os.walk
 def walk(top, onerror=None, followlinks=False):
-
-    top = os.fspath(top)
     dirs = []
     nondirs = []
-    walk_dirs = []
 
     try:
         scandir_it = os.scandir(top)
+        entries = list(scandir_it)
     except OSError as error:
         if onerror is not None:
             onerror(error)
         return
 
-    with scandir_it:
-        while True:
-            try:
-                try:
-                    entry = next(scandir_it)
-                except StopIteration:
-                    break
-            except OSError as error:
-                if onerror is not None:
-                    onerror(error)
-                return
+    for entry in entries:
+        try:
+            is_dir = entry.is_dir()
+        except OSError:
+            is_dir = False
 
-            try:
-                is_dir = entry.is_dir()
-            except OSError:
-                # If is_dir() raises an OSError, consider that the entry is not
-                # a directory, same behaviour than os.path.isdir().
-                is_dir = False
-
-            if is_dir:
-                dirs.append(entry.name)
-            else:
-                nondirs.append(entry.name)
-
+        if is_dir:
+            dirs.append(entry.name)
+        else:
+            nondirs.append(entry.name)
 
     ############################################
-    #yield top, dirs, nondirs
+    # yield top, dirs, nondirs
     if '.git' in dirs:
         path_done.add(os.path.abspath(top))
         print("[GIT]", top)
@@ -60,17 +45,17 @@ def walk(top, onerror=None, followlinks=False):
     # Recurse into sub-directories
     islink, join = os.path.islink, os.path.join
     for dirname in dirs:
-            new_path = join(top, dirname)
-            if followlinks or not islink(new_path):
-                walk(new_path, onerror, followlinks)
+        new_path = join(top, dirname)
+        if followlinks or not islink(new_path):
+            walk(new_path, onerror, followlinks)
 
 
 if __name__ == '__main__':
     # deal with command line path
-    if len(sys.argv) > 1 :
-        paths       = sys.argv[1:]
+    if len(sys.argv) > 1:
+        paths = sys.argv[1:]
         script_path = sys.argv[0]
-    else: # or use current directory as default
+    else:  # or use current directory as default
         paths = ['.']
 
     # iterate each top
